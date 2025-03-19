c46c96e80da3b806c9f3935f0779477cefffde82
seocodes <azanettev@gmail.com>
2025-03-19 17:21:03 -0300

    lista 5 start

--- Lista5/src/Exercicio5.java
+++ Lista5/src/Exercicio5.java
@@ -1,5 +1,34 @@
+import javax.swing.*;
+
 public class Exercicio5 {
     public static void main(String[] args) {
+        String estadoCivil = JOptionPane.showInputDialog("Estado civil: ");
+        classificarEstado(estadoCivil);
+
+    }
+
+    public static void classificarEstado(String estadoCivil){
+        int contS = 0, contC = 0, contV = 0, contD = 0;
+        if(estadoCivil.equalsIgnoreCase("S")){
+            JOptionPane.showMessageDialog(null,"Solteirasso aproveita ai sla");
+            contS++;
+        }
+        else if(estadoCivil.equalsIgnoreCase("C")){
+            JOptionPane.showMessageDialog(null,"Casadera");
+            contC++;
+        }
+        else if(estadoCivil.equalsIgnoreCase("V")){
+            JOptionPane.showMessageDialog(null,"boooh morreu o cara meu viuvo");
+            contV++;
+        }
+        else if(estadoCivil.equalsIgnoreCase("D")){
+            JOptionPane.showMessageDialog(null,"que triste divorciado");
+            contD++;
+        }
+
+        //ISSO PRA CASO FOSSE UM LOOP, SÓ TO MOSTRANDO Q TEM Q SER DENTRO DA FUNÇÃO MESMO, PQ (PESQUISAR NO GPT)
+        JOptionPane.showMessageDialog(null,"Solteiros: "+contS+"\n Casados: "+contC+
+                "\nViúvos: "+contV+"\nDivorciados: "+contD);
 
     }
 }
